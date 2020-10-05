257fa3679603a7a54c5a03411fbb4dc422bb06f0
Juliana <julic.fusco@gmail.com>
2020-10-05 17:02:57 -0300

    começando mais uma

---
+++ TCCF/src/application/FXMLStevenArtOriginsController.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class FXMLStevenArtOriginsController implements Initializable{
+	
+	@FXML
+	protected void sair() {
+		System.exit(0);
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
