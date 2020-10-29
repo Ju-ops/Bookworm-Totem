e8d5e6a00e013d9bc295a80e42b96530ff647c98
Juliana <julic.fusco@gmail.com>
2020-10-29 16:32:45 -0300

    .

--- TCCF/src/ClasseConexao/produto.java
+++ TCCF/src/ClasseConexao/produto.java
@@ -150,36 +150,6 @@ public class produto {
 			return ow; //retorna o valor de ow
 		}
 		return null; 
-	}
-
-	/*public static produto getProduto() {
-		ClasseConexao con = new ClasseConexao(); //instanciando
-		boolean su = con.conectar(); //conecta no banco
-		if (su == false) {
-			throw new Exception("Falha na conexão"); //se a coneccao der errado
-		}
-		PreparedStatement pstmt = con.getConn().prepareStatement("select * from tblProduto where IDProduto = 7"); //mensagem que eu vou mandar pro sql
-		pstmt.setInt(1, id); //o primeiro "?"
-		ResultSet rs = pstmt.executeQuery(); //o resultado do banco
-		if (rs.next()) { // se tiver um pra continuar, ou seja, se existir o produto
-			produto ow = new produto(); //instanciando
-			ow.setIDProduto(rs.getInt("IDProduto")); //fica com o valor dessa tabela
-			ow.setTipoAcervo(rs.getInt("TipoAcervo")); //fica com o valor dessa tabela
-			ow.setNomeLivro(rs.getString("NomeLivro")); //fica com o valor dessa tabela
-			ow.setAutoresLivro(rs.getString("AutoresLivro")); //fica com o valor dessa tabela
-			ow.setSetor(rs.getInt("Setor")); //fica com o valor dessa tabela
-			ow.setFileira(rs.getInt("Fileira")); //fica com o valor dessa tabela
-			ow.setPrateleira(rs.getInt("Prateleira")); //fica com o valor dessa tabela
-			ow.setTipoProduto(rs.getString("TipoProduto")); //fica com o valor dessa tabela
-			ow.setEditora(rs.getString("Editora")); //fica com o valor dessa tabela
-			ow.setDescricaoProd(rs.getString("DescricaoProd")); //fica com o valor dessa tabela
-			ow.setImagemProd(rs.getBytes("ImagemProd")); //fica com o valor dessa tabela
-			ow.setAnoEdicao(rs.getDate("AnoEdicao")); //fica com o valor dessa tabela
-			ow.setDisponiveis(rs.getInt("Disponiveis")); //fica com o valor dessa tabela
-			return ow; //retorna o valor de ow
-		}
-		return null;
-	}*/
-	
+	}	
 
 }
