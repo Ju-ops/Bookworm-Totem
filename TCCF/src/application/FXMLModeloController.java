92c4b00d9bd3198711d8d3d16abffb9a493cf04e
Juliana <julic.fusco@gmail.com>
2020-11-18 16:58:06 -0300

    novo form

--- TCCF/src/application/FXMLModeloController.java
+++ TCCF/src/application/FXMLModeloController.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 public class FXMLModeloController implements Initializable{
 	@FXML
@@ -20,29 +21,25 @@ public class FXMLModeloController implements Initializable{
     @FXML
     private TextField txtPesquisa;
     
+    @FXML
+    private Text lblSinopse;
+    
     produto produtoAtual;
     
-    int x;
-    
     public void setProduto(produto x) {
-    	
-    }
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		try {
-			produtoAtual = produto.getProduto(x);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		lblLocal.setText("Setor:  " + produtoAtual.getSetor() + "      Fileira:  " + produtoAtual.getFileira() + "     Preteleira:  " + produtoAtual.getPrateleira());
+    	produtoAtual = x;
+    	lblLocal.setText("Setor:  " + produtoAtual.getSetor() + "      Fileira:  " + produtoAtual.getFileira() + "     Preteleira:  " + produtoAtual.getPrateleira());
 		lblTituloL.setText(produtoAtual.getNomeLivro());
 		lblAutor.setText(produtoAtual.getAutoresLivro());
 		lblGenero.setText(null);
 		lblAno.setText("Editora: " + produtoAtual.getEditora() + "            Ano Edição: " + produtoAtual.getAnoEdicao() + "");
 		lblDisponiveis.setText("Exemplares disponíveis:  " + produtoAtual.getDisponiveis());
-		//lblSinopse.setText(produtoAtual.getDescricaoProd());
+		lblSinopse.setText(produtoAtual.getDescricaoProd());
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
 	}
 
 }
